638ac29a83f336587784ae4c823b7fa14bb91913
Jack Pettit <skelatorjack@gmail.com>
2020-11-23 15:29:18 -0600

    MIW-1 Basic NavBar
    Added code to NavBar.

--- myinventoryweb/src/App.tsx
+++ myinventoryweb/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
 import React from 'react';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
-      <h1> Hello World</h1>
-      <h1> Hello w </h1>
+      <NavBar />
     </div>
     
   );
--- myinventoryweb/src/components/NavBar.tsx
+++ myinventoryweb/src/components/NavBar.tsx
@@ -1,6 +1,15 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Button, IconButton} from "@material-ui/core";
+import useStyles from "../componentstyles/navbarstyles";
 
 function NavBar() {
+    const navBarStyles = useStyles();
 
+    return (
+        <div className={navBarStyles.root}>
+            <h1> Hello World! </h1>
+        </div>
+    );
 }
 
 export default NavBar;
