173cc426705f6f39ccb94e764d8ac3a741702e4b
Jack Pettit <skelatorjack@gmail.com>
2020-11-29 18:14:15 -0600

    MIW-1 LoginPage
    Added LoginPage to App.

--- myinventoryweb/src/App.tsx
+++ myinventoryweb/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import NavBar from "./components/NavBar";
+import LoginPage from "./components/LoginPage";
 
 function App() {
   return (
     <div>
       <NavBar navBarTitle="MyInventory" />
+      <LoginPage />
     </div>
     
   );
