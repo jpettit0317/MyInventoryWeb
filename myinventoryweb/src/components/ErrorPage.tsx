b20f220292119134a67bab910d0fa7066f4c01e5
Jack Pettit <skelatorjack@gmail.com>
2020-12-07 17:39:46 -0600

    MIW-2 SignUp
    Added an ErrorPage.tsx.

---
+++ myinventoryweb/src/components/ErrorPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+
+// interface ErrorRouterProps {}
+// interface ErrorPageProps extends RouteComponentProps<ErrorRouterProps> {}
+
+// function ErrorPage(): React.FC<ErrorPageProps> {
+//     return (
+//         <div>
+//             <h3> Cannot find page. </h3>
+//         </div>
+//     );
+// }
+
+// export default withRouter(ErrorPage);
