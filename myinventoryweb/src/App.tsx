7130beaeff49b7735365a0ba671e2b73983a21bc
Jack Pettit <skelatorjack@gmail.com>
2020-12-07 17:35:31 -0600

    MIW-2 SignUp
    Added react-router-dom to App and index. This allows the app to use a
    single page.

--- myinventoryweb/src/App.tsx
+++ myinventoryweb/src/App.tsx
@@ -2,12 +2,20 @@ import './App.css';
 import React from 'react';
 import NavBar from "./components/NavBar";
 import LoginPage from "./components/LoginPage";
+import SignUpPage from "./components/SignUpPage";
+import { Route, Switch, BrowserRouter, Link, RouteComponentProps } from 'react-router-dom';
+import LoginPageViewModel from './viewmodels/LoginPageViewModel';
 
 function App() {
+  const loginPageViewModel = LoginPageViewModel.createLoginPageViewModel("", "");  
   return (
     <div>
       <NavBar navBarTitle="MyInventory" />
-      <LoginPage />
+      <Switch>
+        <Route path="/" component={ ()=> <LoginPage loginPageViewModel={loginPageViewModel} /> } />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/signup" component={SignUpPage} />
+      </Switch>
     </div>
     
   );
