fec87293b8d9867f8f73093cffa7fecddb7e01df
Jack Pettit <skelatorjack@gmail.com>
2021-01-18 18:49:21 -0600

    MIW-2 SignUp
    Removed FullName import.
    Replaced string members with UserSignUpInfo.

--- myinventoryweb/src/controllers/SignUpController.ts
+++ myinventoryweb/src/controllers/SignUpController.ts
@@ -1,17 +1,18 @@
+import UserSignUpInfo from "../interfaces/modelinterfaces/UserSignUpInfo";
 import SignUpControllerProps from "../props/SignUpControllerProps";
-import FullName from "../typeDefs/FullName";
 
 class SignUpController {
-    readonly username: string;
-    readonly email: string;
-    readonly password: string;
-    readonly fullName: FullName;
+    private userSignUpInfo: UserSignUpInfo;
 
     private constructor(props: SignUpControllerProps) {
-        this.username = props.username;
-        this.email = props.email;
-        this.password = props.password;
-        this.fullName = props.fullName;
+        this.userSignUpInfo = {
+            email: props.email,
+            username: props.username,
+            password: props.password,
+            firstName: props.fullName.firstName,
+            lastName: props.fullName.lastName,
+            userId: ""
+        };
     }
 
     static createSignUpController(props: SignUpControllerProps): SignUpController {
