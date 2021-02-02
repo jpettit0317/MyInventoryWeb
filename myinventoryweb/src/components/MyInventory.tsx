fce536149998045b0be23b342e8b3838890f98fe
Jack Pettit <skelatorjack@gmail.com>
2021-02-02 13:44:34 -0600

    MIW-3 Item List
    Created a file named myinventorystyles.tsx and moved MyInventory styles to this file.
    Renamed the variable useStyles to useMyInventoryStyles.
    Added function named onAddItemButton pressed.

--- myinventoryweb/src/components/MyInventory.tsx
+++ myinventoryweb/src/components/MyInventory.tsx
@@ -14,60 +14,24 @@ import fs from "fs";
 import logo192 from "../logo192.png";
 import MyInventoryItemCard from "./MyInventoryItemCard";
 import createItems from "../DummyItems";
+import useMyInventoryStyles from "../componentstyles/myinventorystyles";
 
 function MyInventory(): JSX.Element {
-    const useStyles = makeStyles((theme) => ({
-        icon: {
-            marginRight: theme.spacing(2),
-        },
-        heroContent: {
-            backgroundColor: theme.palette.background.paper,
-            padding: theme.spacing(8, 0, 6),
-        },
-        heroButtons: {
-            marginTop: theme.spacing(4),
-        },
-        cardGrid: {
-            paddingTop: theme.spacing(8),
-            paddingBottom: theme.spacing(8),
-        },
-        card: {
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-        },
-        cardMedia: {
-            paddingTop: '56.25%', // 16:9
-        },
-        cardContent: {
-            flexGrow: 1,
-        },
-        footer: {
-            backgroundColor: theme.palette.background.paper,
-            padding: theme.spacing(6),
-        },
-    }));
-    
-    // {
-    //     items.map((item, index) => {
-    //         <Grid item xs={12} sm={6} md={4}>
-    //             <MyInventoryItemCard key={index} item={item} />
-    //         </ Grid>
-    //     })
-    // }
-    const classes = useStyles();
-    
-    function readImage() { 
-        console.log("Logo is " + logo192);       
-    }
-
-    readImage();
-
+    const classes = useMyInventoryStyles();
     const items = createItems();
+
+    function onAddItemButtonPressed() {
+        console.log("Add item button was pressed!");
+    }
 
     return (
         <Container className={classes.cardGrid} maxWidth="md">
             <Grid container spacing={4}>
+                <Grid item xs={12}>
+                    <Button size="small" color="primary" onClick={onAddItemButtonPressed}>
+                        Add Item
+                    </Button>
+                </Grid>
                 {items.map((item, index) => (
                     <Grid item key={index} xs={12} md={4}>
                         <MyInventoryItemCard item={item} />
