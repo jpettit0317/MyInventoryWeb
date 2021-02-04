7bce9296f6a8d42ed5bf1bc588290a3c2cf756bb
Jack Pettit <skelatorjack@gmail.com>
2021-02-04 15:59:07 -0600

    MIW-4 Add Item
    Added redirect to MyInventory.

--- myinventoryweb/src/components/MyInventory.tsx
+++ myinventoryweb/src/components/MyInventory.tsx
@@ -15,17 +15,41 @@ import logo192 from "../logo192.png";
 import MyInventoryItemCard from "./MyInventoryItemCard";
 import createItems from "../DummyItems";
 import useMyInventoryStyles from "../componentstyles/myinventorystyles";
+import { Redirect } from "react-router-dom";
+import RoutePath from "../enums/RoutePath_enum";
 
 function MyInventory(): JSX.Element {
     const classes = useMyInventoryStyles();
     const items = createItems();
 
+    const [shouldRedirect, setShouldRedirect] = useState(false);
+    const [shouldPush, setShouldPush] = useState(true);
+    const [redirectDestination, setRedirectDestination] = useState("");
+
     function onAddItemButtonPressed() {
         console.log("Add item button was pressed!");
+        setRedirect({shouldPush: true, shouldRedirect: true, destination: RoutePath.addItem});
+    }
+
+    function redirectToPage(): JSX.Element {
+        if (shouldPush) {
+            console.log("Pushing to " + redirectDestination);
+            return <Redirect push to={redirectDestination} />;
+        } else {
+            console.log("Replacing with " + redirectDestination);
+            return <Redirect to={redirectDestination} />;
+        }
+    }
+
+    function setRedirect(info: {shouldPush: boolean, shouldRedirect: boolean, destination: string}) {
+        setShouldRedirect(info.shouldRedirect);
+        setShouldPush(info.shouldPush);
+        setRedirectDestination(info.destination);
     }
 
     return (
         <Container className={classes.cardGrid} maxWidth="md">
+            {shouldRedirect === true ? redirectToPage() : ""}
             <Grid container spacing={4}>
                 <Grid item xs={12}>
                     <Button size="small" color="primary" onClick={onAddItemButtonPressed}>
